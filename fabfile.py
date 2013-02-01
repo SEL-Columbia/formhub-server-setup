246298f8a41d5d2239f0e8bc7caa12d8bde28b1b
Ukang'a Dickson <ukanga@gmail.com>
2013-02-01 05:18:33 +0300

    test existence of file before linking or copying - allows multiple runs of server_config

--- fabfile.py
+++ fabfile.py
@@ -90,27 +90,31 @@ def server_setup(deployment_name):
         run('source project_env/bin/activate && pip install -r requirements.pip')
         run('source project_env/bin/activate && python deployer.py')
 
+
 def server_config(deployment_name):
     setup_env(deployment_name)
     nginx_cfg = os.path.join(env.project_directory, 'nginx', 'site.conf')
-    sudo("ln -s %s /etc/nginx/sites-enabled/%s.conf" % (nginx_cfg, deployment_name))
+    sudo("test -f /etc/nginx/sites-enabled/%(site_cfg)s.conf || "
+        "ln -s %(nginx_cfg)s /etc/nginx/sites-enabled/%(site_cfg)s.conf" \
+        % {'nginx_cfg':nginx_cfg, 'site_cfg': deployment_name})
     celeryd_file = os.path.join(env.project_directory, 'celery', 'celeryd')
-    sudo("cp %s /etc/init.d/celeryd" % celeryd_file)
+    sudo("test -f /etc/init.d/celeryd || cp %s /etc/init.d/celeryd" % celeryd_file)
     sudo("chmod +x /etc/init.d/celeryd")
     gunicorn_start_script = os.path.join(
         env.project_directory, 'etc', 'init', 'gunicorn-formhub.conf')
-    sudo("ln -s %s /etc/init/gunicorn-formhub.conf" % (gunicorn_start_script))
+    sudo("test -f /etc/init/gunicorn-formhub.conf || "
+        "ln -s %s /etc/init/gunicorn-formhub.conf" % (gunicorn_start_script))
     celery_cfg = os.path.join(
         env.project_directory, 'etc', 'default', 'celeryd')
     sudo("cp %s /etc/default/celeryd" % (celery_cfg))
     gunicorn_shell_script = os.path.join(
         env.code_src, 'run_gunicorn.sh')
     sudo("chmod +x %s" % (gunicorn_shell_script))
-    sudo('ln -s /lib/init/upstart-job /etc/init.d/gunicorn-formhub')
+    sudo("test -f /etc/init.d/gunicorn-formhub || "
+        "ln -s /lib/init/upstart-job /etc/init.d/gunicorn-formhub")
     sudo('update-rc.d gunicorn-formhub defaults')
     sudo('update-rc.d celeryd defaults')
     sudo('initctl reload-configuration')
     sudo('service nginx restart')
     sudo('service celeryd start')
     sudo('start gunicorn-formhub')
-
